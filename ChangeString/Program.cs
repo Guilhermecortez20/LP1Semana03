d217a577a569f249e0019b8b673b5a3238c0b113
Guilhermecortez20 <Guilhermecortez445@gmail.com>
2025-03-06 23:21:21 +0000

    Adiciona projeto ChangeString para substituir caracteres

---
+++ ChangeString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("String: ");
+            string input = Console.ReadLine() ?? "";
+
+            Console.Write("Caráter: ");
+            char? character = Console.ReadKey().KeyChar;
+            Console.WriteLine(); // Quebra de linha após entrada do caráter
+
+            if (character.HasValue)
+            {
+                string result = input.Replace(character.Value, 'x');
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
+
