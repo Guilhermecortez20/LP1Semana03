a64310b46b7c1d8c4825684d856e9bdbbddb658b
Guilhermecortez20 <Guilhermecortez445@gmail.com>
2025-03-06 22:50:46 +0000

    Adiciona projeto RightSize com verificação de argumentos

---
+++ Rightsize/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RightSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var arg in args)
+            {
+                if (arg.Length >= 8)
+                {
+                    Console.WriteLine("[EARLY STOP]");
+                    return;
+                }
+
+                if (arg.Length > 3)
+                {
+                    Console.WriteLine(arg);
+                }
+            }
+        }
+    }
+}
